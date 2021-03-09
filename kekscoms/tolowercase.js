f3946c5accd2388bdd61b606d0d2c2890b24f13f
FixlTV <fixltvinfo@gmail.com>
2021-03-09 13:40:48 +0100

    ._.

--- kekscoms/tolowercase.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    commands: ['tlc'],
-    modonly: 1,
-    type: 'unlisted',
-    callback: (msg, args, client, serverdata, userdata, config, emotes, color) => {
-        msg.delete()
-        msg.channel.send(args.join(' ').toLowerCase())
-    }
-}
