75899af0e229aca1e4bb822ba98b5e44a4841f77
FixlTV <fixltvinfo@gmail.com>
2021-03-09 16:05:43 +0100

    Bugfix 3.6.3.210309a

--- kekscoms/admin/activate.js
+++ kekscoms/admin/activate.js
@@ -1,17 +1,16 @@
 const fs = require('fs')
-const embeds = require('../../embeds')
+const embeds = require('../embeds')
 const discord = require('discord.js')
 
 module.exports = {
     commands: ['activate'],
-    permissions: 'ADMINISTRATOR',
+    permission: 'ADMINISTRATOR',
     description: 'Aktivert unterschiedliche Add-ons',
     expectedArgs: '[add-on]',
     type: 'admin',
     callback: (msg, args, client, serverdata, userdata, config, emotes, color) => {
         msg.delete()
-        if(!args[0]) args.push('')
-        type = args[0].toLowerCase()
+        type = msg.content.split(' ')[1]
         if(type == 'cookies' || type == 'kekse' || type == 'main') {
             if(serverdata[msg.guild.id]) {
                 embeds.error(msg, "Aktivierung unmöglich", "Das KeksSystem ist bereits aktiv.")
@@ -22,43 +21,34 @@ module.exports = {
                     serverdata[msg.guild.id].lv = 1
                     serverdata[msg.guild.id].xp = 0
                     serverdata[msg.guild.id].prefix = "-"
-                    fs.writeFileSync('../serverdata.json',JSON.stringify(serverdata, null, 2))
+                    fs.writeFileSync('./serverdata.json',JSON.stringify(serverdata, null, 2))
                     embeds.success(msg, 'System aktiviert!', 'Das **KeksSystem** wurde erfolgreich aktiviert.')
                 } catch (err) {
                     embeds.error(msg,'Fehler',err,"\nBitte wende dich an ein Teammitglied, um dieses Problem zu melden.")
                 }}
-            } else if(type == 'randomping' || type == 'anyone' || type == '@anyone' || type == '@someone') {
-                if(serverdata[msg.guild.id]) {
-                    if(serverdata[msg.guild.id].anyone) {
-                        embeds.error(msg, "Aktivierung unmöglich", "Das Add-on **KeksBot Randomping** ist bereits aktiv.")
+            } else {
+                if(type == 'randomping' || type == 'anyone' || type == '@anyone' || type == '@someone') {
+                    if(serverdata[msg.guild.id]) {
+                        if(serverdata[msg.guild.id].anyone) {
+                            embeds.error(msg, "Aktivierung unmöglich", "Das Add-on **KeksBot Randomping** ist bereits aktiv.")
+                        } else {
+                            serverdata[msg.guild.id].anyone = 1
+                            fs.writeFileSync('./serverdata.json', JSON.stringify(serverdata, null, 2))
+                            embeds.success(msg, "System aktiviert!", "Das **KeksBot Randomping** Add-on wurde erfolgreich aktiviert.")
+                        }
                     } else {
-                        serverdata[msg.guild.id].anyone = 1
-                        fs.writeFileSync('../serverdata.json', JSON.stringify(serverdata, null, 2))
-                        embeds.success(msg, "System aktiviert!", "Das **KeksBot Randomping** Add-On wurde erfolgreich aktiviert.")
+                        embeds.error(msg, "Aktivierung unmöglich", "Das KeksSystem ist auf diesem Server noch nicht aktiv.\nBitte verwende zunächst `activate main`, um es zu starten.")
                     }
                 } else {
-                    embeds.error(msg, "Aktivierung unmöglich", "Das KeksSystem ist auf diesem Server noch nicht aktiv.\nBitte verwende zunächst `-activate main`, um es zu starten.")
+                    var embed = new discord.MessageEmbed()
+                        .setColor(color.yellow)
+                        .setTitle('Systemübersicht')
+                        .setDescription('Hier sind alle Befehle, mit denen sich die KeksBot Add-ons aktivieren lassen.')
+                        .addField('KeksBot Main', '`activate main`\nAktiviert die Standart KeksBot Funktionen. Ohne die geht gar nichts.')
+                        .addField('KeksBot Randompings', '`activate randomping`\nAktiviert das Randomping Add-on.\nAus Performancegründen ist diese Funktion aktuell ausschließlich für Partner.\nBei einem @anyone in einer Nachricht, wird ein zufälliges Mitglied des Servers gepingt.')
+                        .setFooter(`© KeksBot ${config.version}`, client.user.avatarURL())
+                    msg.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }))
                 }
-            } else {
-                var anyone = ''
-                var mod = ''
-                if(serverdata[msg.guild.id]) {
-                    if(serverdata[msg.guild.id].anyone) anyone = '\nDieses System ist aktuell aktiv.'
-                    if(serverdata[msg.guild.id].mod) mod = '\nDieses System ist aktuell aktiv.'
-                }
-                var embed = new discord.MessageEmbed()
-                    .setColor(color.normal)
-                    .setTitle('Systemübersicht')
-                    .setDescription('Hier sind alle Add-Ons aufgelistet. Nutze `<de>activate <Add-On>` um das jeweilige System zu (de)aktivieren.')
-                    .addField('Main', 'Die normalen KeksBot Funktionen.\nDie Aktivierung ist nur nötig, wenn die automatische Aktivierung bei Serverbeitritt fehlgeschlagen ist.', true)
-                    .addField('@anyone', `Das Randomping Add-On.\nWenn eine Nachricht \`@anyone\`, \`@someone\`, oder \`@random\` enthält, wird ein zufälliger Nutzer gepingt.${anyone}`, true)
-                    .addField('Moderation', `Die KeksBot Moderationsfeatures.\n**Diese Funktion ist aktuell noch in der Alpha Phase und nicht zur allgemeinen Nutzung zugelassen.**${mod}`, true)
-                    .setFooter(`© KeksBot ${config.version}`, client.user.avatarURL())
-                msg.channel.send(embed).then(msg =>         
-                    setTimeout(msg => {
-                        if(!msg.deleted) {msg.delete()}
-                    }, 30000, msg)
-                )
             }
     }
 }
