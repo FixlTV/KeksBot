f445645e67d8ba5b0df3cde6160be551adbed51e
FixlTV <fixltvinfo@gmail.com>
2021-04-29 12:54:47 +0200

    Bugfix activate 2

--- kekscoms/admin/activate.js
+++ kekscoms/admin/activate.js
@@ -10,7 +10,8 @@ module.exports = {
     type: 'admin',
     callback: (msg, args, client, serverdata, userdata, config, emotes, color) => {
         msg.delete()
-        type = args[0].toLowerCase() || ''
+        type = ''
+        if (args[0]) type = args[0].toLowerCase()
         if(type == 'cookies' || type == 'kekse' || type == 'main') {
             if(serverdata[msg.guild.id]) {
                 embeds.error(msg, "Aktivierung unmöglich", "Das KeksSystem ist bereits aktiv.")
