a6c76f9e72b2200b42737cf40489fd52de165a00
FixlTV <fixltvinfo@gmail.com>
2021-07-19 22:39:06 +0200

    Hab ne 4 in Franz

--- commands/admin/checkperms.js
+++ commands/admin/checkperms.js
@@ -6,5 +6,11 @@ module.exports = {
     description: 'Überprüft die Berechtigungen des Bots. Sinnvoll, wenn Fehler beim ausführen mancher Commands auftreten.',
     async callback(msg, args, client, serverdata, userdata, config, emotes, color) {
         let deleteMessages = msg.guild.me.hasPermission('DELETE_MESSAGES')
+        /*
+            Hewwu lieber Leser,
+            Wenn du diesen Text NACH dem 25.07.2021 im neusten Commit siehst,
+            zögere bitte nicht mich auf Discord mit Pings volzuspammen.
+            Vielen Dank :3
+        */
     }
 }
